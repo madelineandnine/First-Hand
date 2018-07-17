504a7715eea58de81ea3cef8f61a517e2b87febd
Maddie <maddiewood42@gmail.com>
2018-07-17 18:45:02 -0400

    server testing

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ if (process.env.NODE_ENV === 'production') {
 	// app.get('/', (req, res) => {
 	// 	res.sendFile(path.join(__dirname, '../build/'))
 	// })
-	app.use(express.static('client/build'));
+	app.use(express.static('client/build/public'));
 }
 
 /* Express app ROUTING */
