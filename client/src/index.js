5a9df3dfeebc585f34f8eca742cf27d35632001c
Maddie <maddiewood42@gmail.com>
2018-07-17 18:22:19 -0400

    react router fix.

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,6 @@ import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
 
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
+ReactDOM.render(	
+	<App />, document.getElementById('root')
 )
